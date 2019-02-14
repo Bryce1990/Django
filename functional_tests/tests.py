1b15e006779180ce7b8f7ef897fefb2d271d6b4a
Kevin Bryce <brycekev@outlook.com>
2019-02-14 17:01:52 -0500

    Multi User

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -25,40 +25,9 @@ class NewVisitorTest(LiveServerTestCase):
                 return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
-                    raise # -*- coding: utf-8 -*-
+                    raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes to the hompage
-        self.browser.get(self.live_server_url)
-
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        #[...rest of comments as before]
-
-        inputbox.send_keys('Buy peacock feathers')
-
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        inputbox=self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -77,14 +46,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
+
 
         # Francis visits the home page. There is no sign of Ediths's list
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
+
 
         # Francis starts a new list by entering a new item. He is less intersting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
